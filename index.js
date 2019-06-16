cf5ac9179eef23889a21d3dcb50821e3610e5b89
Misael <misael.garay@tech4trade.com>
2019-06-15 20:33:29 -0500

    Added all data endpoint, added sync feature

--- index.js
+++ index.js
@@ -6,23 +6,52 @@ var bodyParser = require('body-parser');
 PouchDB.plugin(require('pouchdb-find'));
 
 var db = new PouchDB('http://couchadmin:admin@localhost:5984/products');
-	db.info()
-	.then(info => console.log(info))
-	.catch(e => console.log(e));
+db.info()
+.then(info => console.log(info))
+.catch(e => console.log(e));
+
+var localDB = new PouchDB('products');	
+localDB.info(info => {
+	console.log("Local Database");
+	console.log(info);
+}).catch(e => console.log(e));
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
-app.get('/createDB',(req,res) => {
+app.get('/home',(req,res) => {
 	res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.get('/getProducts',(req,res) => {
-	db.get()
-	.then(docs => cosole.log(docs))
-	.catch(e => console.log(e));
+	localDB.allDocs({ include_docs: true, descending: true })
+	.then(docs => {
+		let products = null;
+		if(docs && docs.rows){
+			products = docs.rows.map(item => {
+				return item.doc;
+			});
+		}
+		res.send({
+			success : true,
+			data : products 
+		});
+	})
+	.catch(e => res.send({
+		success : false,
+		...e
+	}));
+});
+
+app.get('/syncData',(req, res)=>{
+	localDB.sync(db).on('complete',() => 
+		res.send({ success : true })
+	).on('error', error => res.send({
+		success : false,
+		...error
+	}));
 });
 
 app.post('/createProduct',(req,res) => {
@@ -30,11 +59,11 @@ app.post('/createProduct',(req,res) => {
 			...req.body, 
 			_id : req.body.name
 		};
-	var response = {
+	let response = {
 		data : data,
 		success : true
 	};
-	db.put(data)
+	localDB.put(data)
 	.then(info => res.send(response))
 	.catch(e => res.send({error : "Couldnt save it"}));
 });
